6eaf11626d287e604773b7f4a7410f5a4c37c346
Peyton Doyle <p6doyle@gmail.com>
2019-08-04 11:48:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,160 +1,307 @@
-require 'pry'
+# Hashketball Nests
+#
+# Great news! You're going to an NBA game! The only catch is that you've been
+# volunteered to keep stats at the game.
+#
+# Using Nested Hashes, define a game, with two teams, their players, and the players stats:
+#
+# The game has two teams.
+#
+# A team has:
+# - A name
+# - Two colors
+#
+# Each team should have at least 5 players
+#
+# Each player should have a:
+# - name
+# - number (like their jersey number)
+# - shoe size
+#
+# Each player should have the following stats:
+# - points
+# - rebounds
+# - assists
+# - steals
+# - blocks
+# - slam dunks
 
-def game_hash
-  {
-    home: {
-      team_name: "Brooklyn Nets",
-      colors: ["Black", "White"],
-      players: [
-        {
-          player_name: "Alan Anderson",
-          number: 0,
-          shoe: 16,
-          points: 22,
-          rebounds: 12,
-          assists: 12,
-          steals: 3,
-          blocks: 1,
-          slam_dunks: 1
-        }, {
-          player_name: "Reggie Evans",
-          number: 30,
-          shoe: 14,
-          points: 12,
-          rebounds: 12,
-          assists: 12,
-          steals: 12,
-          blocks: 12,
-          slam_dunks: 7
-        }, {
-          player_name: "Brook Lopez",
-          number: 11,
-          shoe: 17,
-          points: 17,
-          rebounds: 19,
-          assists: 10,
-          steals: 3,
-          blocks: 1,
-          slam_dunks: 15
-        }, {
-          player_name: "Mason Plumlee",
-          number: 1,
-          shoe: 19,
-          points: 26,
-          rebounds: 12,
-          assists: 6,
-          steals: 3,
-          blocks: 8,
-          slam_dunks: 5
-        }, {
-          player_name: "Jason Terry",
-          number: 31,
-          shoe: 15,
-          points: 19,
-          rebounds: 2,
-          assists: 2,
-          steals: 4,
-          blocks: 11,
-          slam_dunks: 1
+hashketball = {
+  :home => {
+    :team_name => "Monstars",
+    :colors => ["black", "green"],
+    :players => {
+      
+      "Pound" => {
+        :number => 99,
+        :shoe_size => 10,
+        :stats => {
+          :points => 40,
+          :rebounds => 11,
+          :assists => 12,
+          :steals => 8,
+          :blocks => 9,
+          :slam_dunks => 6
         }
-      ]
+      },
+
+      "Bupkus" => {
+        :number => 95,
+        :shoe_size => 11,
+        :stats => {
+          :points => 32,
+          :rebounds => 16,
+          :assists => 8,
+          :steals => 12,
+          :blocks => 15,
+          :slam_dunks => 8
+        }
+      },
+
+      "Bang" => {
+        :number => 93,
+        :shoe_size => 12,
+        :stats => {
+          :points => 24,
+          :rebounds => 9,
+          :assists => 13,
+          :steals => 10,
+          :blocks => 6,
+          :slam_dunks => 3
+        }
+      },
+
+      "Blanko" => {
+        :number => 88,
+        :shoe_size => 14,
+        :stats => {
+          :points => 10,
+          :rebounds => 12,
+          :assists => 8,
+          :steals => 6,
+          :blocks => 4,
+          :slam_dunks => 0
+        }
+      },
+
+      "Nawt" => {
+        :number => 85,
+        :shoe_size => 13,
+        :stats => {
+          :points => 18,
+          :rebounds => 4,
+          :assists => 10,
+          :steals => 3,
+          :blocks => 9,
+          :slam_dunks => 7
+        }
+      },
     },
-    away: {
-      team_name: "Charlotte Hornets",
-      colors: ["Turquoise", "Purple"],
-      players: [
-        {
-          player_name: "Jeff Adrien",
-          number: 4,
-          shoe: 18,
-          points: 10,
-          rebounds: 1,
-          assists: 1,
-          steals: 2,
-          blocks: 7,
-          slam_dunks: 2
-        }, {
-          player_name: "Bismak Biyombo",
-          number: 0,
-          shoe: 16,
-          points: 12,
-          rebounds: 4,
-          assists: 7,
-          steals: 7,
-          blocks: 15,
-          slam_dunks: 10
-        }, {
-          player_name: "DeSagna Diop",
-          number: 2,
-          shoe: 14,
-          points: 24,
-          rebounds: 12,
-          assists: 12,
-          steals: 4,
-          blocks: 5,
-          slam_dunks: 5
-        }, {
-          player_name: "Ben Gordon",
-          number: 8,
-          shoe: 15,
-          points: 33,
-          rebounds: 3,
-          assists: 2,
-          steals: 1,
-          blocks: 1,
-          slam_dunks: 0
-        }, {
-          player_name: "Brendan Haywood",
-          number: 33,
-          shoe: 15,
-          points: 6,
-          rebounds: 12,
-          assists: 12,
-          steals: 22,
-          blocks: 5,
-          slam_dunks: 12
+  },
+  :away => {
+    :team_name => "Toon Squad",
+    :colors => ["white", "blue"],
+    :players => {
+      
+      "Bugs" => {
+        :number => 1,
+        :shoe_size => 10,
+        :stats => {
+          :points => 30,
+          :rebounds => 13,
+          :assists => 10,
+          :steals => 10,
+          :blocks => 8,
+          :slam_dunks => 5
         }
-      ]
-    }
+      },
+
+      "Babs" => {
+        :number => 3,
+        :shoe_size => 6,
+        :stats => {
+          :points => 22,
+          :rebounds => 13,
+          :assists => 8,
+          :steals => 10,
+          :blocks => 12,
+          :slam_dunks => 3
+        }
+      },
+
+      "Taz" => {
+        :number => 5,
+        :shoe_size => 5,
+        :stats => {
+          :points => 28,
+          :rebounds => 7,
+          :assists => 15,
+          :steals => 4,
+          :blocks => 8,
+          :slam_dunks => 0
+        }
+      },
+
+      "Daffy" => {
+        :number => 7,
+        :shoe_size => 15,
+        :stats => {
+          :points => 14,
+          :rebounds => 12,
+          :assists => 8,
+          :steals => 6,
+          :blocks => 10,
+          :slam_dunks => 3
+        }
+      },
+
+      "Jordan" => {
+        :number => 23,
+        :shoe_size => 12,
+        :stats => {
+          :points => 40,
+          :rebounds => 8,
+          :assists => 4,
+          :steals => 12,
+          :blocks => 10,
+          :slam_dunks => 12
+        }
+      },
+    },
   }
-end
+}
 
-def num_points_scored(player_name)
+# Using the power of Ruby, and the Hashes you created above, answer the following questions:
+# Return the number of points scored for any player:
 
-  # SOLUTION 1 - naive
-  # some_hash.each do |key, value|
-  # game_hash.each do |team, team_data|
-  #   team_data.each do |attr, data|
-  #     if attr == :players
-  #       found_player = data.find do |player|
-  #         player[:player_name] == player_name
-  #       end
-  #
-  #       if found_player
-  #         return found_player[:points]
-  #       end
-  #     end
-  #   end
-  # end
-
-  # SOLUTION 2 - good
-  # game_hash.each do |team, team_data|
-  #   team_data[:players].each do |player|
-  #     if player[:player_name] == player_name
-  #       return player[:points]
-  #     end
-  #   end
-  # end
-
-  # SOLUTION 3
-  # get a list of all the players
-  all_players = game_hash.values.collect do |team|
-    team[:players]
-  end.flatten
-
-  # find the player whose name matches the argument 'player_name'
-  # return that player's points
-  all_players.each do |player|
-    return player[:points] if player[:player_name] == player_name
+def points_per_player(player, hashketball)
+  player.capitalize!
+  if hashketball[:home][:players].include?(player)
+   hashketball[:home][:players][player][:stats][:points]
+  elsif hashketball[:away][:players].include?(player)
+   hashketball[:away][:players][player][:stats][:points]
+  else
+   "No player found."
   end
 end
+
+puts points_per_player("blanko", hashketball)
+
+#
+
+# Return the shoe size for any player:
+
+def player_shoe_size(player, hashketball)
+  player.capitalize!
+  if hashketball[:home][:players].include?(player)
+   hashketball[:home][:players][player][:shoe_size]
+  elsif hashketball[:away][:players].include?(player)
+   hashketball[:away][:players][player][:shoe_size]
+  else
+   "No player found."
+  end
+end
+
+puts player_shoe_size("taz", hashketball)
+
+
+#
+
+# Return both colors for any team:
+
+def team_colors(team, hashketball)
+  sym = team.to_sym
+  if hashketball.include?(sym)
+   hashketball[sym][:colors]
+  else
+   "Try home or away."
+  end
+end
+
+puts team_colors("away", hashketball)
+
+#
+
+# Return both teams names:
+
+def team_names(hashketball)
+  puts "The #{hashketball[:home][:team_name]} welcome the #{hashketball[:away][:team_name]}"
+  puts 
+end
+
+puts team_names(hashketball)
+
+#
+
+# Return all the player numbers for a team:
+
+def player_nums(team, hashketball)
+    sym = team.to_sym
+    player_numbers = []
+    
+      hashketball[sym][:players].each_value do |x|
+      player_numbers << x[:number]
+  end
+    player_numbers
+end
+
+puts player_nums("away", hashketball)
+
+
+# Return all the stats for a player:
+
+def player_stats(player_name, hashketball)
+  player_name.capitalize!
+  if hashketball[:home][:players].include?(player_name)
+    hashketball[:home][:players][player_name][:stats]
+  elsif hashketball[:away][:players].include?(player_name)
+    hashketball[:away][:players][player_name][:stats]
+  else
+    "No player found."
+  end
+end
+
+puts player_stats("Daffy", hashketball)
+
+#
+
+# Return the rebounds for the player with the largest shoe size
+
+def biggest_shoe(hashketball)
+  player_number_with_biggest_shoe = ""
+  biggest_shoe_size = 0
+  
+  hashketball[:home][:players].each do |name, player|
+    if player[:shoe_size] > biggest_shoe_size
+      player_number_with_biggest_shoe = name
+      biggest_shoe_size = player[:shoe_size]
+    end
+  end
+  hashketball[:away][:players].each do |name, player|
+    if player[:shoe_size] > biggest_shoe_size
+      player_number_with_biggest_shoe = name
+      biggest_shoe_size = player[:shoe_size]
+    end
+  end
+  
+  " #{player_number_with_biggest_shoe} : #{biggest_shoe_size} "
+end
+
+puts biggest_shoe(hashketball)
+  
+
+#
+
+# Bonus Questions: define methods to return the answer to the following questions:
+# Which player has the most points?
+#
+
+# Which team has the most points?
+#
+
+# Which player has the longest name?
+#
+
+# Super Bonus:
+# Write a method that returns true if the player with the longest name had the most steals:
+
